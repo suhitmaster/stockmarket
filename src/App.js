f9e57e0ecab80e211b653efde3a1bbba0b907926
suhit.k <suhit.k@hcl.com>
2018-11-05 13:32:30 +0530

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Home from './Home';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { nav } from 'reactstrap';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <ul className="App-menu">
-              <li> <img src={logo} className="App-logo" alt="logo" /></li>
-              <li className="App-menuItem">Home</li>
-          </ul>          
-        </header>
+      <div>
         
-        <Home />
-      </div>
-    );
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+         <img src={logo} className="App-logo" alt="logo" />
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/Home">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Home">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>  
+
+  
+
+<div class="container">
+  <h1 class="mt-4">Logo Nav by Start Bootstrap</h1>
+  <Home />
+</div>
+
+</div>        
+        
+            );
   }
 }
 
